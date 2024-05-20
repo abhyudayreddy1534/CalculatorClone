c5ec7dcbb0f44f09bbac9a79a1d241c55ecabbe2
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-05-20 11:10:42 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sravanthi Chinthireddy on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
